71889df95ce1f44e4b4cf82eec42c7b5e4e9f2ed
Gerardo Vitale <gerardo.vitale.errico@gmail.com>
2022-07-30 23:56:38 +0200

    logger config file path added as a constant in constant.py

--- ml-models/src/ml_models/config/constants.py
+++ ml-models/src/ml_models/config/constants.py
@@ -1,3 +1,6 @@
+DATA_PATH = '/app/data/creditcard.csv'
+LOGGER_CONFIG_FILE = '/app/ml_models/config/log_config.yml'
+
+# ML params
 TEST_SIZE = 0.33
 RANDOM_STATE = 42
-DATA_PATH = '/app/data/creditcard.csv'
--- ml-models/src/ml_models/config/logger.py
+++ ml-models/src/ml_models/config/logger.py
@@ -3,9 +3,11 @@ import logging.config
 
 import yaml
 
+from ml_models.config.constants import LOGGER_CONFIG_FILE
+
 
 def get_logger(name: str = __name__) -> logging.Logger:
-    with open('/app/ml_models/config/log_config.yml', 'r') as f:
+    with open(LOGGER_CONFIG_FILE, 'r') as f:
         config = yaml.safe_load(f.read())
         logging.config.dictConfig(config)
     return logging.getLogger(name=name)
