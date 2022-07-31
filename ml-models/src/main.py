70aeea6f96ce01b694c644c3cd49a60258e38abc
Gerardo Vitale <gerardo.vitale.errico@gmail.com>
2022-07-31 18:55:34 +0200

    added a third experiment RUS + LogReg

--- ml-models/src/main.py
+++ ml-models/src/main.py
@@ -1,7 +1,8 @@
 from imblearn.over_sampling import RandomOverSampler, SMOTE
+from imblearn.under_sampling import RandomUnderSampler
 from sklearn.linear_model import LogisticRegression
 
-from ml_models.config.constants import RANDOM_STATE
+from ml_models.config.constants import RANDOM_STATE, N_JOBS
 from ml_models.config.logger import get_logger
 from ml_models.experiment import execute_experiment
 from ml_models.load import load_data
@@ -16,19 +17,30 @@ def main() -> None:
 
     # ==================== ==================== ==================== ==================== ====================
     # ROS + LogisticRegression
-    logger.debug('Starting experiment')
+    experiment_id = 'ROS + LogReg'
+    logger.debug('[{0}] Starting experiment'.format(experiment_id))
     ros = RandomOverSampler(random_state=RANDOM_STATE)
-    log_reg = LogisticRegression(random_state=RANDOM_STATE, max_iter=1000, n_jobs=-1)
-    execute_experiment('ROS + LogReg', X, y, ros, log_reg)
-    logger.debug('Experiment finished')
+    log_reg = LogisticRegression(random_state=RANDOM_STATE, max_iter=1000, n_jobs=N_JOBS)
+    execute_experiment(experiment_id, X, y, ros, log_reg)
+    logger.debug('[{0}] Experiment finished'.format(experiment_id))
+
+    # ==================== ==================== ==================== ==================== ====================
+    # RUS + LogisticRegression
+    experiment_id = 'RUS + LogReg'
+    logger.debug('[{0}] Starting experiment'.format(experiment_id))
+    ros = RandomUnderSampler(random_state=RANDOM_STATE)
+    log_reg = LogisticRegression(random_state=RANDOM_STATE, max_iter=1000, n_jobs=N_JOBS)
+    execute_experiment(experiment_id, X, y, ros, log_reg)
+    logger.debug('[{0}] Experiment finished'.format(experiment_id))
 
     # ==================== ==================== ==================== ==================== ====================
     # SMOTE + LogisticRegression
-    # logger.debug('Starting experiment')
-    # smote = SMOTE(sampling_strategy='auto', random_state=RANDOM_STATE, k_neighbors=5, n_jobs=-1)
-    # log_reg = LogisticRegression(random_state=RANDOM_STATE, max_iter=1000, n_jobs=-1)
-    # execute_experiment('SMOTE + LogReg', X, y, smote, log_reg)
-    # logger.debug('Experiment finished')
+    experiment_id = 'SMOTE + LogReg'
+    logger.debug('[{0}] Starting experiment'.format(experiment_id))
+    smote = SMOTE(sampling_strategy='auto', random_state=RANDOM_STATE, k_neighbors=5, n_jobs=N_JOBS)
+    log_reg = LogisticRegression(random_state=RANDOM_STATE, max_iter=1000, n_jobs=N_JOBS)
+    execute_experiment(experiment_id, X, y, smote, log_reg)
+    logger.debug('[{0}] Experiment finished'.format(experiment_id))
 
 
 if __name__ == '__main__':
