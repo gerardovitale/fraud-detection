4a6825a90a760cc23532302109dbccfc5d0a1b1d
Gerardo Vitale <gerardo.vitale.errico@gmail.com>
2022-08-24 23:58:30 +0200

    metric data model updated

--- ml-models/src/ml_models/metrics.py
+++ ml-models/src/ml_models/metrics.py
@@ -1,10 +1,14 @@
 import json
 from logging import Logger
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Union
 
 from imblearn.metrics import geometric_mean_score
 from pandas import Series
 from sklearn.metrics import f1_score, precision_score, recall_score, roc_auc_score
+
+
+def create_message(metric_id: str, metrics: Union[dict, list]):
+    return {'message': {'metric_id': metric_id, 'data': metrics}}
 
 
 def get_data_metrics(logger: Logger, y: Series, subset: str, res_method: str = None) -> List[Dict[str, Any]]:
@@ -16,7 +20,7 @@ def get_data_metrics(logger: Logger, y: Series, subset: str, res_method: str = N
         'is_resampled': True if res_method else False,
         'res_method': res_method if res_method else '',
     } for i in y.unique()]
-    logger.info(json.dumps(metrics))
+    logger.info(create_message('dataframe', metrics))
     return metrics
 
 
@@ -28,7 +32,7 @@ def get_model_metrics(logger: Logger, y_test: Series, y_pred: Series) -> Dict[st
         'g_mean': float(geometric_mean_score(y_test, y_pred)),
         'roc_auc_score': float(roc_auc_score(y_test, y_pred)),
     }
-    logger.info(json.dumps(metrics))
+    logger.info(create_message('model', metrics))
     return metrics
 
 
@@ -41,5 +45,5 @@ def publish_model_metric_comparison(logger: Logger, model_metrics: Dict[str, flo
             'basic_points': model_metrics_res[metric] - model_metrics[metric],
             'percentage': (model_metrics_res[metric] - model_metrics[metric]) / model_metrics[metric]
         }
-    logger.info(json.dumps(metrics_comparison))
+    logger.info(create_message('comparison', metrics_comparison))
     return metrics_comparison
